128a21215b105a3fde222cb19e7477d47cb758b3
lgwk42 <luisgunkim@gmail.com>
2024-09-19 20:15:56 +0900

    :sparkles: :: query dsl config add

---
+++ src/main/java/com/project/welspyserverv3/global/config/QueryDSLConfig.java
@@ -0,0 +1,20 @@
+package com.project.welspyserverv3.global.config;
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDSLConfig {
+
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
