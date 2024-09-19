0325459692945331b069fb6bf28e1154d70abb38
lgwk42 <luisgunkim@gmail.com>
2024-09-19 19:53:01 +0900

    :tada: :: create project

---
+++ src/main/java/com/project/welspyserverv3/WelspyServerV3Application.java
@@ -0,0 +1,13 @@
+package com.project.welspyserverv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelspyServerV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelspyServerV3Application.class, args);
+    }
+
+}
