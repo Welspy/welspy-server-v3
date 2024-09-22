daba41bf42d2e781784089b66c7353ae3bf5c499
lgwk42 <luisgunkim@gmail.com>
2024-09-22 20:56:32 +0900

    :sparkles: :: room exception add

---
+++ src/main/java/com/project/welspyserverv3/domain/room/exception/RoomNotFoundException.java
@@ -0,0 +1,15 @@
+package com.project.welspyserverv3.domain.room.exception;
+
+import com.project.welspyserverv3.domain.room.exception.error.RoomError;
+import com.project.welspyserverv3.domain.user.exception.error.UserError;
+import com.project.welspyserverv3.global.exception.BusinessException;
+
+public class RoomNotFoundException extends BusinessException {
+
+    public static final RoomNotFoundException EXCEPTION = new RoomNotFoundException();
+
+    private RoomNotFoundException(){
+        super(RoomError.ROOM_NOT_FOUND);
+    }
+
+}
