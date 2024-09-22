b67bbe16e8e36abac37675c1abc86bbea1fb2f8e
lgwk42 <luisgunkim@gmail.com>
2024-09-22 20:56:44 +0900

    :sparkles: :: room dto add

---
+++ src/main/java/com/project/welspyserverv3/domain/room/client/dto/Room.java
@@ -0,0 +1,43 @@
+package com.project.welspyserverv3.domain.room.client.dto;
+
+import com.project.welspyserverv3.domain.room.domain.entity.RoomEntity;
+import com.project.welspyserverv3.domain.room.domain.enums.Category;
+import com.project.welspyserverv3.domain.room.domain.enums.RoomType;
+import com.project.welspyserverv3.domain.user.client.dto.User;
+import com.project.welspyserverv3.domain.user.domain.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Builder
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+
+    private Long roomId;
+    private String title;
+    private String description;
+    private Long goalMoney;
+    private int memberLimit;
+    private Category category;
+    private RoomType roomType;
+
+    public Room toRoom(RoomEntity roomEntity) {
+        return Room.builder()
+                .roomId(roomEntity.getRoomId())
+                .title(roomEntity.getTitle())
+                .description(roomEntity.getDescription())
+                .goalMoney(roomEntity.getGoalMoney())
+                .memberLimit(roomEntity.getMemberLimit())
+                .category(roomEntity.getCategory())
+                .roomType(roomEntity.getRoomType())
+                .build();
+    }
+
+}
