396fcf050e33803a14c9fb1c053de5201069f02c
lgwk42 <luisgunkim@gmail.com>
2024-09-22 21:09:26 +0900

    :sparkles: :: member entity/dto add

---
+++ src/main/java/com/project/welspyserverv3/domain/room/client/dto/MemberList.java
@@ -0,0 +1,31 @@
+package com.project.welspyserverv3.domain.room.client.dto;
+
+import com.project.welspyserverv3.domain.room.domain.entity.MemberListEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Builder
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberList {
+
+    private Long idx;
+    private Long roomId;
+    private String email;
+
+    public MemberList toMemberList(MemberListEntity memberListEntity) {
+        return MemberList.builder()
+                .idx(memberListEntity.getIdx())
+                .roomId(memberListEntity.getRoomId())
+                .email(memberListEntity.getEmail())
+                .build();
+    }
+
+}
---
+++ src/main/java/com/project/welspyserverv3/domain/room/domain/entity/MemberListEntity.java
@@ -0,0 +1,27 @@
+package com.project.welspyserverv3.domain.room.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@Table(name = "tb_member_list")
+public class MemberListEntity {
+
+    @Id
+    private Long idx;
+
+    @Column(nullable = false)
+    private Long roomId;
+
+    @Column(nullable = false)
+    private String email;
+
+}
