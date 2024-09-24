4693a7363f5e7c7ed834aea660e40f83f5fec2bd
lgwk42 <luisgunkim@gmail.com>
2024-09-24 14:44:38 +0900

    :fire: :: delete unused import

--- src/main/java/com/project/welspyserverv3/domain/user/client/api/UserController.java
+++ src/main/java/com/project/welspyserverv3/domain/user/client/api/UserController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
