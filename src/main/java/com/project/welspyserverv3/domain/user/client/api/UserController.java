16f854595e4d4f2c16080d81dca6529ec550aa99
lgwk42 <luisgunkim@gmail.com>
2024-09-24 14:40:29 +0900

    :sparkles: :: get user profile api add

---
+++ src/main/java/com/project/welspyserverv3/domain/user/client/api/UserController.java
@@ -0,0 +1,30 @@
+package com.project.welspyserverv3.domain.user.client.api;
+
+import com.project.welspyserverv3.domain.user.client.dto.User;
+import com.project.welspyserverv3.domain.user.service.UserService;
+import com.project.welspyserverv3.global.common.dto.response.BaseResponseData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@Tag(name = "유저 API")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    @Operation(summary = "유저 프로필 조회", description = "유저 프로필을 토큰으로 조회합니다.")
+    public BaseResponseData<User> getUser(){
+        return BaseResponseData.ok(
+                "유저 프로필 조회 성공",
+                userService.getUser()
+        );
+    }
+
+}
