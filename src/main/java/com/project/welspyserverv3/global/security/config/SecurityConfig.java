a04b3d4d323aeff68ae73e7d44d222a76d852ccb
lgwk42 <luisgunkim@gmail.com>
2024-09-25 10:57:16 +0900

    :memo: :: cors config fix

--- src/main/java/com/project/welspyserverv3/global/security/config/SecurityConfig.java
+++ src/main/java/com/project/welspyserverv3/global/security/config/SecurityConfig.java
@@ -65,19 +65,4 @@ public class SecurityConfig{
         return urlBasedCorsConfigurationSource;
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
-    }
-
 }
