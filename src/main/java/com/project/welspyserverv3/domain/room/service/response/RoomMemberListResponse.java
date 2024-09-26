e4636c86968355ecf4d05b9bec98b867d4a1dd98
lgwk42 <luisgunkim@gmail.com>
2024-09-26 15:13:52 +0900

    :memo: :: room member list constructor add

--- src/main/java/com/project/welspyserverv3/domain/room/service/response/RoomMemberListResponse.java
+++ src/main/java/com/project/welspyserverv3/domain/room/service/response/RoomMemberListResponse.java
@@ -1,5 +1,6 @@
 package com.project.welspyserverv3.domain.room.service.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class RoomMemberListResponse {
 
     private Long idx;
