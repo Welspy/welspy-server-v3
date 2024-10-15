b5852a75c23db98c599c4f2be8600617f406c3dc
lgwk42 <luisgunkim@gmail.com>
2024-10-15 17:33:19 +0900

    :memo: :: room id res args add

--- src/main/java/com/project/welspyserverv3/domain/ai/client/dto/RoomIdResponse.java
+++ src/main/java/com/project/welspyserverv3/domain/ai/client/dto/RoomIdResponse.java
@@ -1,10 +1,14 @@
 package com.project.welspyserverv3.domain.ai.client.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomIdResponse {
 
     private Long roomId;
