d444cad9b6692f0dd5f22d6cd68778f6e721d7da
lgwk42 <luisgunkim@gmail.com>
2024-10-15 11:24:15 +0900

    :fire: :: JwtAuthenticationEntryPoint log delete

--- src/main/java/com/project/welspyserverv3/global/security/jwt/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/project/welspyserverv3/global/security/jwt/handler/JwtAuthenticationEntryPoint.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 
