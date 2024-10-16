f40e6cf1a55c6b06f08aa87768d37831d66deed7
lgwk42 <luisgunkim@gmail.com>
2024-10-16 14:04:53 +0900

    :sparkles: :: image delete api add

--- src/main/java/com/project/welspyserverv3/domain/s3/api/S3Controller.java
+++ src/main/java/com/project/welspyserverv3/domain/s3/api/S3Controller.java
@@ -1,12 +1,15 @@
 package com.project.welspyserverv3.domain.s3.api;
 
 import com.project.welspyserverv3.domain.s3.service.S3Service;
+import com.project.welspyserverv3.global.common.dto.response.BaseResponse;
 import com.project.welspyserverv3.global.common.dto.response.BaseResponseData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,6 +29,13 @@ public class S3Controller {
         return BaseResponseData.ok(
                 "이미지 업로드 성공",
                 profileImage);
+    }
+
+    @DeleteMapping
+    @Operation(summary = "이미지 삭제")
+    public BaseResponse s3Delete(@RequestParam String imageUrl){
+        s3Service.deleteImageFromS3(imageUrl);
+        return BaseResponse.ok("이미지 삭제 성공");
     }
 
 }
