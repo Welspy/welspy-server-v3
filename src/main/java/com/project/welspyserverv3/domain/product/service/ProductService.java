a194d0bf9cfe567894fa807f149445b61189ba4f
lgwk42 <luisgunkim@gmail.com>
2024-10-22 15:14:28 +0900

    :memo: :: discount fix

--- src/main/java/com/project/welspyserverv3/domain/product/service/ProductService.java
+++ src/main/java/com/project/welspyserverv3/domain/product/service/ProductService.java
@@ -6,6 +6,7 @@ import com.project.welspyserverv3.domain.product.domain.entity.ProductEntity;
 import com.project.welspyserverv3.domain.product.domain.repository.jpa.ProductJpaRepository;
 import com.project.welspyserverv3.domain.product.exception.ProductNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,14 +16,18 @@ public class ProductService {
     private final ProductJpaRepository productJpaRepository;
     private final Product productDto;
 
-    public void createProduct(CreateProductRequest request) {
+    public void createProduct(CreateProductRequest request) throws BadRequestException {
         Long price = request.getPrice();
+        Long discountedPrice = price - (price * request.getDiscount() / 100);
+        if (discountedPrice < 0) {
+            throw new BadRequestException("할인된 가격은 음수일 수 없습니다");
+        }
         productJpaRepository.save(ProductEntity.builder()
                 .name(request.getName())
                 .description(request.getDescription())
                 .price(request.getPrice())
                 .discount(request.getDiscount())
-                .discountedPrice(price - (price * request.getDiscount() / 100))
+                .discountedPrice(discountedPrice)
                 .imageUrl(request.getImageUrl())
                 .productUrl(request.getProductUrl())
                 .build()
