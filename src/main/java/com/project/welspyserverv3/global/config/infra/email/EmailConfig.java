38a11771b46de63a6d128a499d055b4a8901f74c
lgwk42 <luisgunkim@gmail.com>
2024-10-29 16:03:34 +0900

    :bulb: :: remove comment

--- src/main/java/com/project/welspyserverv3/global/config/infra/email/EmailConfig.java
+++ src/main/java/com/project/welspyserverv3/global/config/infra/email/EmailConfig.java
@@ -1,30 +1,29 @@
 package com.project.welspyserverv3.global.config.infra.email;
 
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
-
 @Configuration
 public class EmailConfig {
 
-    /* set important data */
-    @Value("${spring.mail.username}") private String username;
-    @Value("${spring.mail.password}") private String password;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
 
     @Bean
     public JavaMailSender mailSender() {
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587); // TLS port
+        mailSender.setPort(587);
         mailSender.setUsername(username);
         mailSender.setPassword(password);
 
-        /* Use Properties Object to set JavaMailProperties */
         Properties javaMailProperties = new Properties();
         javaMailProperties.put("mail.transport.protocol", "smtp");
         javaMailProperties.put("mail.smtp.auth", "true");
@@ -32,7 +31,7 @@ public class EmailConfig {
         javaMailProperties.put("mail.smtp.starttls.enable", "true");
         javaMailProperties.put("mail.debug", "true");
         javaMailProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-        javaMailProperties.put("mail.smtp.ssl.protocols", "TLSv1.3"); // TLS v1.3을 사용
+        javaMailProperties.put("mail.smtp.ssl.protocols", "TLSv1.3");
 
         mailSender.setJavaMailProperties(javaMailProperties);
 
